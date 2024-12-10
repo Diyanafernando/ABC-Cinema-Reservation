e0bac91bf121fcf618304b9866a2c078f35e654c
SingithKesara <singithkesarawahalathanthri@gmail.com>
2024-12-10 19:50:05 +0530

    I give up

--- src/main/java/com/abc/BookingServlet.java
+++ src/main/java/com/abc/BookingServlet.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 @WebServlet("/book")
 public class BookingServlet extends HttpServlet {
--- src/main/java/com/abc/PaymentServlet.java
+++ src/main/java/com/abc/PaymentServlet.java
@@ -1,5 +1,12 @@
 package com.abc;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @WebServlet("/pay")
 public class PaymentServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
